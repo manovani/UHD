329dc25c692176afca32bb7e5904d9bcbd0c7609
manovani <149648920+manovani@users.noreply.github.com>
2024-08-16 13:43:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://graph.org/file/2bf5bb32bbc4ee87099d7.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/df85b2b9b5666d04d9b43.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/096dc02c579812ce5d468.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/83f15a0073df393225fe6.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/df85b2b9b5666d04d9b43.jpg")
 
 # Admins, Channels & Users
